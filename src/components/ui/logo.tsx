44e904a390e3236091a557425f6bf7eb9a04a7f9
ERoydev <e.roydev@gmail.com>
2025-04-25 21:34:22 +0300

    feat(logo): logo

---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,12 @@
+import PassChainLogo from "../../assets/images/PassChainLogo.png"
+
+
+function Logo({ className, ...props }: React.ComponentProps<"div">) {
+    return (
+      <div className={className} {...props}>
+        <img src={PassChainLogo} alt="Logo" className="w-30 h-30" />
+      </div>
+    );
+}
+
+export { Logo}
